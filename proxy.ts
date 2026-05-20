a08e44c7205590f0e34976b5c18322c55d098807
niihash <50837663+niihash@users.noreply.github.com>
2026-05-19 23:29:16 -0300

    feat: add routes proxy

--- proxy.ts
+++ proxy.ts
@@ -1,20 +1,40 @@
 import { updateSession } from "@/lib/supabase/proxy";
-import { type NextRequest } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export async function proxy(request: NextRequest) {
-  return await updateSession(request);
+    const response = await updateSession(request);
+
+    const isAuthPage = request.nextUrl.pathname.startsWith("/login") || request.nextUrl.pathname.startsWith("/register");
+
+    const isProtectedRoute = request.nextUrl.pathname.startsWith("/dashboard") || request.nextUrl.pathname.startsWith("/meals") || request.nextUrl.pathname.startsWith("/fasting")
+
+    const hasSession = request.cookies.get("sb-access-token");
+
+    if (isProtectedRoute && !hasSession) {
+        return NextResponse.redirect(
+            new URL("/login", request.url)
+        );
+    }
+
+    if (isAuthPage && hasSession) {
+        return NextResponse.redirect(
+            new URL("/dashboard", request.url)
+        );
+    }
+
+    return response;
 }
 
 export const config = {
-  matcher: [
-    /*
-     * Match all request paths except:
-     * - _next/static (static files)
-     * - _next/image (image optimization files)
-     * - favicon.ico (favicon file)
-     * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
-     * Feel free to modify this pattern to include more paths.
-     */
-    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
-  ],
+    matcher: [
+        /*
+         * Match all request paths except:
+         * - _next/static (static files)
+         * - _next/image (image optimization files)
+         * - favicon.ico (favicon file)
+         * - images - .svg, .png, .jpg, .jpeg, .gif, .webp
+         * Feel free to modify this pattern to include more paths.
+         */
+        "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+    ],
 };
